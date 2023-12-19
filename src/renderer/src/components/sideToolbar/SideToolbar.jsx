2c68306675ba42e86407e5601ec3018b1b6f4ef5
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2023-12-18 22:43:03 -0500

    feat: add tabs to side toolbar for future use

--- src/renderer/src/components/sideToolbar/SideToolbar.jsx
+++ src/renderer/src/components/sideToolbar/SideToolbar.jsx
@@ -1,10 +1,23 @@
 import "../../assets/sideToolbar/SideToolbar.css";
 import EditorSizeForm from "./EditorSizeForm";
+import { useState } from "react";
 
 const SideToolbar = () => {
+  const [currentTab, setCurrentTab] = useState("editor");
+
+  const tabHandler = (tab) => setCurrentTab(tab);
+
   return (
     <div id="sideToolbar-container">
-      <EditorSizeForm />
+      <div id="sideToolbar-tab-container">
+        <div className="sideToolbar-tab" onClick={() => tabHandler("editor")}>
+          Editor
+        </div>
+        <div className="sideToolbar-tab" onClick={() => tabHandler("test")}>
+          Test
+        </div>
+      </div>
+      {currentTab === "editor" && <EditorSizeForm />}
     </div>
   );
 };
